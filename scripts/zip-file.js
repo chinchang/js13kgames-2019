3f52312b449b6ccbc32388efcda92a0bba89b6e6
Kushagra Gour <chinchang457@gmail.com>
2019-09-03 18:30:17 +0530

    separate js into file and add build process

---
+++ scripts/zip-file.js
@@ -0,0 +1,12 @@
+// Author: https://github.com/mtmckenna/js13kgames-parcel-starter
+const fs = require("fs");
+const archiver = require("archiver");
+
+const distDir = process.cwd() + "/dist";
+const output = fs.createWriteStream(distDir + "/game.zip");
+const archive = archiver("zip", { zlib: { level: 9 } });
+
+archive.pipe(output);
+archive.file(distDir + "/index.html", { name: "index.html" });
+
+archive.finalize();
